c1ffa88ebb2f002fafeae95f5fcf2ebc7f15cb35
Ulises Alanis <ulises.alanis.255@gmail.com>
2026-07-02 14:19:52 -0600

    improved tmx command

--- fish/functions/tmx.fish
+++ fish/functions/tmx.fish
@@ -1,5 +1,8 @@
-function tmx --wraps=tmux --description 'alias tmx tmux'
-    set v (pwd)
-    set v (string replace -a "$HOME/" '' $v)
-    tmux $argv new -s $v
+function tmx --wraps=tmux --description 'Attach to or create a tmux session named after the current directory'
+    set v $(string replace -a "." "_"  $(string replace -a "$HOME/" '' $(pwd)))
+    if tmux list-sessions | grep -q $v 
+        tmux attach-session -t $v
+    else
+        tmux $argv new -s $v
+    end
 end
